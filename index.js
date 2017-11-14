bf07546c690b1493c0d4c0149c260df688da5c1c
Precious Okwu <preciousokwu@outlook.com>
2017-11-14 13:57:06 +0100

    changed port and add new route

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var express=require("express")
+var express=require("express");
 var app=express();
 
 app.get("/",function(req,res){
         res.send("Welcome to Landing Page");
-}).listen(3005,function(){
-    console.log("Listening to Port 3005")
+}).listen(3006,function(){
+    console.log("Listening to Port 3006")
+});
+
+
+app.get("/dashboard",function(req,res){
+        res.send("Welcome to Dashboard Page");
 });
